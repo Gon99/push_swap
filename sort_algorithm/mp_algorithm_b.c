7ecb90100b89a5be80b107ac896d9f755c46661c
Gonzalo Liano ricoy <goliano-@c2r2s3.42madrid.com>
2021-10-18 16:42:42 +0200

    psw-100

--- sort_algorithm/mp_algorithm_b.c
+++ sort_algorithm/mp_algorithm_b.c
@@ -6,7 +6,7 @@
 /*   By: goliano- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 11:55:14 by goliano-          #+#    #+#             */
-/*   Updated: 2021/10/15 16:47:22 by goliano-         ###   ########.fr       */
+/*   Updated: 2021/10/18 16:42:08 by goliano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,16 @@ int	b_is_sorted(t_stack *n_stack)
 	return (is_sorted);
 }
 
-static void	handle_two_b(t_stack *n_stack, int *chunk)
+/*static void	handle_two_b(t_stack *n_stack, int *chunk)
 {
 	if (n_stack->stack_b[0] < n_stack->stack_b[1])
 		do_sb(n_stack);
 	do_pa(n_stack);
 	do_pa(n_stack);
 	*chunk -= 2;
-}
+}*/
 
-static void	do_second_half_b(t_stack *n_stack, t_sstack *s_stack, int *chunk)
+/*static void	do_second_half_b(t_stack *n_stack, t_sstack *s_stack, int *chunk)
 {
 	int	crb;
 	int	nta;
@@ -67,17 +67,128 @@ static void	do_second_half_b(t_stack *n_stack, t_sstack *s_stack, int *chunk)
 		do_rrb(n_stack);
 		crb--;
 	}
+}*/
+
+static int	get_max_val_of_chunk(t_stack *n_stack)
+{
+	int	max;
+	int	i;
+
+	max = n_stack->stack_b[0];
+	i = 1;
+	while (i < n_stack->l_b)
+	{
+		if (n_stack->stack_b[i] > max)
+			max = n_stack->stack_b[i];
+		i++;
+	}
+	return (max);
 }
 
-static void	do_mp_b(t_stack *n_stack, t_sstack *s_stack, int *chunk, int nc)
+static int	idx_of_max_val(t_stack *n_stack)
 {
-	ft_memcpy(s_stack->ss, n_stack->stack_b, *chunk);
-	s_stack->l = *chunk;
-	sort_n_stack(s_stack);
-	if (nc == 1)
-		do_last_chunk_b(n_stack, s_stack, chunk);
+	int	i;
+	int	idx;
+	int	max;
+
+	max = n_stack->stack_b[0];
+	i = 1;
+	idx = 0;
+	while (i < n_stack->l_b)
+	{
+		if (n_stack->stack_b[i] > max)
+		{
+			max = n_stack->stack_b[i];
+			idx = i;
+		}
+		i++;
+	}
+	return (idx);
+}
+
+static int	idx_of_min_val(t_stack *n_stack)
+{
+	int	i;
+	int	idx;
+	int	min;
+
+	min = n_stack->stack_b[0];
+	i = 1;
+	idx = 0;
+	while (i < n_stack->l_b - 1)
+	{
+		if (n_stack->stack_b[i] > min)
+		{
+			min = n_stack->stack_b[i];
+			idx = i;
+		}
+		i++;
+	}
+	return (idx);
+	
+}
+
+static int	get_min_val(t_stack *n_stack)
+{
+	int	min;
+	int	i;
+
+	i = 1;
+	min = n_stack->stack_b[0];
+	while (i < n_stack->l_b - 1)
+	{
+		if (n_stack->stack_b[i] < min)
+			min = n_stack->stack_b[i];
+		i++;
+	}
+	return (min);
+}
+
+static void	do_mp_b(t_stack *n_stack, int *chunk)
+{
+	int	max;
+	int	idx;
+	int	min;
+	int	idx2;
+
+	idx = idx_of_max_val(n_stack);
+	max = get_max_val_of_chunk(n_stack);
+	min = get_min_val(n_stack);
+	idx2 = idx_of_min_val(n_stack);
+	/*printf("IDX: %d\n", idx);
+	printf("MAX:  %d\n", max);
+	printf("LB:  %d\n", n_stack->l_b / 2);
+	printf("LB2:  %d\n", n_stack->l_b);
+	*/
+	if (idx < n_stack->l_b / 2)
+	{
+		while (1)
+		{
+			if (n_stack->stack_b[0] == max)
+			{
+				do_pa(n_stack);
+				*chunk = *chunk - 1;
+				break ;
+			}
+			else
+				do_rb(n_stack);
+		}
+	}
 	else
-		do_second_half_b(n_stack, s_stack, chunk);
+	{
+		while (1)
+		{
+			if (n_stack->stack_b[n_stack->l_b - 1] == max)
+			{
+				do_rrb(n_stack);
+				do_pa(n_stack);
+				*chunk = *chunk - 1;
+				break;
+			}
+			else
+				do_rrb(n_stack);
+		}
+	}
 }
 
 void	mp_algorithm_b(t_stack *n_stack, t_sstack *s_stack)
@@ -88,9 +199,9 @@ void	mp_algorithm_b(t_stack *n_stack, t_sstack *s_stack)
 	s_stack->l = 0;
 	nc = n_of_chunks(n_stack);
 	chunk = 0;
-	while (nc > 0)
+	while (n_stack->l_b > 0)
 	{
-		if (chunk == 0)
+		/*if (chunk == 0)
 			chunk = get_chunk(n_stack, nc);
 		if (chunk == 1)
 		{
@@ -100,7 +211,8 @@ void	mp_algorithm_b(t_stack *n_stack, t_sstack *s_stack)
 		else if (chunk == 2)
 			handle_two_b(n_stack, &chunk);
 		else
-			do_mp_b(n_stack, s_stack, &chunk, nc);
+			do_mp_b(n_stack, &chunk);*/
+		do_mp_b(n_stack, &chunk);
 		if (chunk == 0)
 			nc--;
 	}
